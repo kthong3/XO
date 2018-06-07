1185bc80c51d1069136bf3f6ceb6956bdf6f073e
kthong3 <kthongsakounh@gmail.com>
2018-06-07 13:39:45 -0500

    refactor and pass all tests

--- xo.rb
+++ xo.rb
@@ -1,10 +1,4 @@
-def xo(string)
-  if !string.downcase.include?('x') && !string.downcase.include?('o')
-    return true
-  else
-    array = string.downcase.split("")
-    x = array.select {|letter| letter == 'x' }
-    o = array.select {|letter| letter == 'o' }
-    x.count == o.count
-  end
+def xo(str)
+  string = str.downcase
+  string.count('x') == string.count('o')
 end
